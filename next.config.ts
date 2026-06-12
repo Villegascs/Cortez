32564e293d8d8037bb5047f64feddd104d901c9e
Villegas <ftsaevc@gmail.com>
2026-06-12 15:54:06 -0400

    Allow i.ibb.co image domains in next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
